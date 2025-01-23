6ee3937a27b9f02cce712cd40a6663643f6b1a8e
Carlos Villegas <136353783+carlosvilleg@users.noreply.github.com>
2025-01-23 23:09:04 +0000

    initial

---
+++ versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    scaffolding = {
+      source = "app.terraform.io/carlos-villegas-sandbox/scaffolding"
+      version = "0.0.0"
+    }
+  }
+}
+
+provider "scaffolding" {
+  # Configuration options 
+}
